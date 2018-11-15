a30d89fe0b3e1b073b52292aace6e133a6b6c782
lijinghao1997 <lijinghao1997@outlook.com>
2018-11-15 13:54:15 +0800

    渐隐渐显Header实现完成

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
         <detail-bannner></detail-bannner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBannner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-      DetailBannner
+      DetailBannner,
+      DetailHeader,
   }
 }
 </script>
 
 <style lang='stylus' scoped>
-
+    .content
+      height : 50rem
 </style>
 
--- src/pages/home/components/Recommand.vue
+++ src/pages/home/components/Recommand.vue
@@ -2,8 +2,7 @@
     <div>
         <div class="recommand-title">热销推荐</div>
         <ul>
-          
-            <li class="item border-bottom" v-for="item of list" :key="item.id">
+            <router-link tag="li" to="'/detail'+item.id" class="item border-bottom" v-for="item of list" :key="item.id">
                 <img class="item.img" :src="item.imgUrl">
                 <div class="item-info">
                     <p class="item-title">{{item.title}}</p>
@@ -12,8 +11,7 @@
                     <button class="item-button">查看详情</button>
                     </router-link>
                 </div>
-            </li>
-          
+            </router-link>      
         </ul>
     </div>
 </template>
